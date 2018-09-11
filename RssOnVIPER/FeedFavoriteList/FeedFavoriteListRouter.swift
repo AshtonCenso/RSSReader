0660e7de15ff872ba30841eec7aa91154563e6ea
Anton Tsykanov <antontsykanov@MacBook-Pro-Anton.local>
2018-09-11 18:52:00 +0300

    added favorite

---
+++ RssOnVIPER/FeedFavoriteList/FeedFavoriteListRouter.swift
@@ -0,0 +1,9 @@
+//
+//  FeedFavoriteListRouter.swift
+//  RssOnVIPER
+//
+//  Created by Anton Tsykanov on 11.09.2018.
+//  Copyright © 2018 Anton Tsykanov. All rights reserved.
+//
+
+import Foundation
